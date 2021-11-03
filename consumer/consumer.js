6e5885972d067e29e34bd0be61edaffe2ad5e097
Vova Nagornyj <vova-nagornyj@mail.ru>
2021-11-03 18:54:11 +0700

    add rmq to delete users from bd

---
+++ consumer/consumer.js
@@ -0,0 +1,68 @@
+const ampq = require('amqplib/callback_api');
+const {Sequelize, DataTypes} = require("sequelize");
+
+const Connect = new Sequelize(
+    'host',
+    'host',
+    'gqallxxx79311',
+    {
+        dialect: 'postgres',
+        host: 'ripbacke-db-1',
+        port: 5432,
+        logging: false
+    }
+);
+
+const User = Connect.define(
+    'User',
+    {
+        firstName:{
+            type: DataTypes.STRING
+        },
+        lastName:{
+            type: DataTypes.STRING
+        },
+        password:{
+            type: DataTypes.STRING
+        }
+    }
+);
+
+async function deleteFromBD(id){
+    try{
+        const data = await Connect.models.User.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        await data.destroy()
+    }catch (err) {
+        console.log(`что то пошло совсем не так: ${err}`);
+    }
+}
+
+
+ampq.connect('amqp://rabbitmq:5672', (err, connection)=> {
+    if(err){
+        throw err;
+    }
+    connection.createChannel((err, channel)=>{
+        if(err){
+            throw err;
+        }
+
+        let queueName = "DeleteRequests";
+
+        channel.assertQueue(queueName, {
+            durable: false
+        });
+
+        channel.consume(queueName, async (msg)=>{
+            console.log(`Message received: ${msg.content.toString()}`)
+            await deleteFromBD(parseInt(msg.content))
+            channel.ack(msg)
+        })
+
+    })
+})
